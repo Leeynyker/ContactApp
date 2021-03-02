66bd209d6fdc3d3f418e768858af2091bdefd1bb
Leey <leeynykeramr@gmail.com>
2021-03-01 20:30:25 -0500

    Pass

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 # Mysql Connection
 app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'Leey'
-app.config['MYSQL_PASSWORD'] = 'andres601'
+app.config['MYSQL_USER'] = ''
+app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'flaskcontacts'
 
 mysql = MySQL(app)
